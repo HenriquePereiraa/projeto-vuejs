4d03db60431ff6100d62e6831bc19aed3843b8e7
Gustavo Raposo <ghsr@ecomp.poli.br>
2021-04-06 15:22:52 -0300

    contato - iniciando

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Banner/>
     <Sobre/>
+    <Contato/>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner';
 import Sobre from './components/Sobre';
+import Contato from './components/Contato';
 
 export default {
   name: 'App',
   components: {
     Banner,
     Sobre,
+    Contato,
   }
 }
 </script>
