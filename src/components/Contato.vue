72a7d520c20156125e2d92b3557beb2d9dc02f81
Gustavo Raposo <ghsr@ecomp.poli.br>
2021-04-06 22:23:19 -0300

    contato - em andamento(20%)

--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,5 +1,34 @@
 <template>
     <div id="Contato">
-        
+        <h1>Entre em contato</h1>
+        <form>
+
+        </form>
     </div>
 </template>
+
+<style scoped>
+
+    /* BODY */
+    #Contato {
+        height: 931px;
+        width: 100%;
+        background-image: url("../assets/bg_02.png");
+        background-repeat: no-repeat;
+        background-position: center bottom;
+    }
+
+    /* TITULO */
+    #Contato h1 {
+        margin: 76px 0px 200px 70px;  
+        color: #349075;
+    }
+
+    /* FORMULARIO */
+    #Contato form {
+        width: 100%;
+        height: 200px;
+        display: flex;
+        background-color: black;
+    }
+</style>
