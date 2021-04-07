a328c0275d5b9f4dfa84d6494ab4be378540d723
Gustavo Raposo <ghsr@ecomp.poli.br>
2021-04-07 18:36:03 -0300

    em andamento(50%)

--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,9 +1,18 @@
 <template>
     <div id="Contato">
         <h1>Entre em contato</h1>
-        <form>
-
-        </form>
+        <div id="main_area">
+            <form>
+                <input type="text" placeholder="Nome">
+                <input type="text" placeholder="E-mail">
+                <textarea placeholder="deixe uma mensagem"></textarea>
+                <button type="submit">enviar</button>
+            </form>
+            <div>
+                <span>Telefone: (99)9999-9999</span><br><br>
+                <span>Endereço: Avenida pernambucana, 123 Edificio Stark 3° Andar/Sala 123 - Recife/PE</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,16 +28,61 @@
     }
 
     /* TITULO */
-    #Contato h1 {
-        margin: 76px 0px 200px 70px;  
+    #Contato > h1 {
+        position: relative;
+        top: 75px;
+        left: 50px;  
         color: #349075;
     }
 
-    /* FORMULARIO */
-    #Contato form {
+    /* MAIN AREA */
+    #main_area {
+        position: relative;
+        top: 385px;
+        height: 466px;
         width: 100%;
-        height: 200px;
         display: flex;
-        background-color: black;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    /* MAIN AREA > FORMULÁRIO */
+    #main_area > form {
+        width: 600px;
+        display: flex;
+        flex-direction: column;
+        border-bottom: 1px solid black;
+    }
+    #main_area > form input {
+        height: 40px;
+        width: 600px;
+        border-radius: 15px;
+        border: 0px;
+        padding-inline: 20px;
+        margin: 10px 0px 10px 0px;
+    }
+    #main_area > form textarea {
+        height: 100px;
+        width: 600px;
+        padding: 20px; 
+        border-radius: 15px;
+        border: 0px;
+        margin: 10px 0px 10px 0px;
+    }
+    #main_area > form button {
+        height: 31px;
+        width: 140px;
+        text-align: center;
+        background-color: #31C399;
+        color: white;
+        border-radius: 15px;
+        border: 0px;
+        margin: 10px 0px 70px 0px;
+    }
+    #main_area > div {
+        width: 600px;
+        padding-top: 5px;
+        padding-left: 5px;
+        font-size: 16px;
     }
 </style>
